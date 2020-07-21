c9cb1806b8abcc599e7a93b335e52de430400de2
Everton-Luiss <elssufpa@gmail.com>
2020-07-21 11:02:36 -0300

    new

--- test_CSV.py
+++ test_CSV.py
@@ -1,23 +1,33 @@
-#CSV is going to be constantly updated by an outside source
 import random
 from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-x_vals=[]
-y_vals=[]
+plt.style.use('fivethirtyeight')
 
-index=count()
-def animated(i):
-    x_vals.append(next(index))
-    y_vals.append(random.randint(0,5))
+x_vals = []
+y_vals = []
 
-    plt.cla()#cla method garante que o grafico fique da mesma cor
-    plt.plot(x_vals, y_vals)
+index = count()
 
-ani = FuncAnimation(plt.gcf(), animated, interval=20)
 
+def animate(i):
+    data = pd.read_csv('data.csv')
+    x = data['x_value']
+    y1 = data['total_1']
+    y2 = data['total_2']
+
+    plt.cla()
+
+    plt.plot(x, y1, label='Channel 1')
+    plt.plot(x, y2, label='Channel 2')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 
 plt.tight_layout()
 plt.show()
